93124da3f877ac8d2f07054440208c1bb2d55414
Geo D. Diaz <jorge.diaz17@tectijuana.edu.mx>
2021-03-11 17:24:00 -0800

    Practice_1 Part_2

--- Practice_1.scala
+++ Practice_1.scala
@@ -1,6 +1,4 @@
-// Assessment 1/Practica 1
-
-  
+// Assessment 1/Practica 1  
     //1. Desarrollar un algoritmo en scala que calcule el radio de un círculo
     
     val Pi = 3.1416
@@ -38,3 +36,23 @@
     
     val bird = "tweet"
     printf(s"Estoy escribiendo un $bird")
+
+    //4. Dada la variable mensaje = "Hola Luke yo soy tu padre!" utiliza slice para extraer la
+    //   secuencia "Luke"
+
+    val mensaje = "Hola Luke yo soy tu padre!"
+
+    mensaje.slice (5,9)
+
+    //5. Cual es la diferencia entre value y una variable en scala?
+
+    //La variable val NO se puede modificar una vez que ha sido predefinido su valor.
+
+    //La variable var SÍ se pueden sobreescribir sus valores, se puede modificar. 
+    //(Únicamente se puede sobrescribir si el dato a ingresar es el mismo tipo de dato que el anterior)
+
+    //6. Dada la tupla (2,4,5,1,2,3,3.1416,23) regresa el número 3.1416 
+
+    val tupla = (2,4,5,1,2,3,3.1416,23)
+
+    (tupla._7)
