cb1d2fb261edefd67ffb5402fd29261f95d89761
Geo D. Diaz <jorge.diaz17@tectijuana.edu.mx>
2021-03-15 14:55:42 -0700

    Practice_2 Part_1

--- Practice_2.scala
+++ Practice_2.scala
@@ -10,6 +10,7 @@ var ele5 = "perla" :: ele4
 // 3. Traer los elementos de "lista" "verde", "amarillo", "azul"
 ele5 slice (2, 5)
 // 4. Crea un arreglo de numero en rango del 1-1000 en pasos de 5 en 5
+var Arreglo = Array(Range(1,1000, 5))
 // 5. Cuales son los elementos unicos de la lista Lista(1,3,3,4,6,7,3,7) utilice conversion a conjuntos
 // 6. Crea una mapa mutable llamado nombres que contenga los siguiente
 //     "Jose", 20, "Luis", 24, "Ana", 23, "Susana", "27"
