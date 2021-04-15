262efa75f5c6ba0a4e00bb7955197f853cd17076
Geo D. Diaz <jorge.diaz17@tectijuana.edu.mx>
2021-04-15 12:31:22 -0700

    Evaluation Part Two

--- Evaluation/EvaluationUnit1.scala
+++ Evaluation/EvaluationUnit1.scala
@@ -40,3 +40,40 @@ hvRatio.show()
 val dfdias = df.withColumn("Dia", dayofmonth(df("Date"))) //Se crea una nueva columna para el dataframe para manipular los dias
 val dias = dfdias.groupBy("Dia").max() //Agrupamiento para el dataframe de todos los dias
 dias.select($"Dia", $"max(Close)").sort(desc("max(Close)")).show(1) //Ordenamiento de mayor a menor mostrando el primer registro con el valor mas alto
+
+//9. Escribe con tus propias palabras en un comentario de tu código. ¿Cuál es el
+//significado de la columna Cerrar “Close”?
+
+//La columna Close se refiere al precio de la acción que tuvo al final de cerrar con el dia
+
+//10. ¿Cuál es el máximo y mínimo de la columna “Volume”?
+df.select(max("Volume"), min("Volume")).show()
+
+//11.Con Sintaxis Scala/Spark $ conteste los siguiente:
+/*◦ Hint: Basicamente muy parecido a la session de dates, tendran que crear otro
+dataframe para contestar algunos de los incisos.*/
+
+//a. ¿Cuántos días fue la columna “Close” inferior a $ 600?
+
+df.filter($"Close" < 600).count()
+
+//b. ¿Qué porcentaje del tiempo fue la columna “High” mayor que $ 500?
+
+var High500: Double = df.filter($"High" > 500).count()
+(High500 / df.count())*100
+
+//c. ¿Cuál es la correlación de Pearson entre columna “High” y la columna “Volumen”?
+
+df.select(corr("High","Volume")).show()
+
+//d. ¿Cuál es el máximo de la columna “High” por año?
+
+val dfyear = df.withColumn("Year", year(df("Date")))
+val year = dfyear.groupBy("Year").max()
+year.select($"Year", $"max(High)").show()
+
+//e. ¿Cuál es el promedio de columna “Close” para cada mes del calendario?
+
+val dfmes = df.withColumn("Mes", month(df("Date")))
+val mes = dfmes.groupBy("Mes").mean()
+mes.select($"Mes", $"avg(Close)").show()
