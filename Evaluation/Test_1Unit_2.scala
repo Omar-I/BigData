88bae93deb25d563550663fd6ac071f67e84b957
Geo D. Diaz <jorge.diaz17@tectijuana.edu.mx>
2021-05-31 21:24:12 -0700

    Evaluation Part Two

--- Evaluation/Test_1Unit_2.scala
+++ Evaluation/Test_1Unit_2.scala
@@ -39,3 +39,65 @@ data.show(5)
 //'Describe' hace una analítica básica de los datos
 
 data.describe().show()
+
+//6. Haga la transformación pertinente para los datos categoricos los cuales serán
+//nuestras etiquetas a clasificar.
+/* 'VectorAssembler' combina todas las columnas del array en una sola columna de vector*/
+
+val assembler = new VectorAssembler().setInputCols(Array("sepal_length","sepal_width","petal_length","petal_width")).setOutputCol("features")
+val asmb = assembler.transform(data)
+asmb.show()
+
+// 'StringIndexer' Reune una columna de cadenas y los establece en índices
+val labelIndexer = new StringIndexer().setInputCol("species").setOutputCol("indexedspecies").fit(data)
+val lblInd = new StringIndexer().setInputCol("species").setOutputCol("indexedspecies")
+val indx = lblInd.fit(data).transform(data)
+indx.show()
+
+println(s"Found species: ${labelIndexer.labels.mkString("[", ", ", "]")}")
+
+// Se transforma el labelIndexer en el dataframe principal 'data' con las nuevas columnas renombrando la columna de los indices y lo transforma en un nuevo dataframe
+val indexed = labelIndexer.transform(data).withColumnRenamed("indexedSpecies", "label")
+val features = assembler.transform(indexed)
+features.show()
+
+/*'StringIndexer' ordena las columnas de la cadena de acuerdo con la frecuencia de ocurrencia.
+Los datos de entrada serán "label" y los de salida serán "indexedSpecies" */
+val featureIndexer = new StringIndexer().setInputCol("label").setOutputCol("indexedSpecies").fit(indexed)
+
+//Asignamos un 60% de los datos para entrenamiento y un 40% para hacer pruebas.
+//Asignamos una seed random para los datos.
+
+val splits = features.randomSplit(Array(0.6, 0.4), seed = 1234L)
+val train = splits(0)
+val test = splits(1)
+
+/*Aquí vamos a establecer nuestra red neuronal con 4 capas:
+4 neuronas de entrada
+2 capas ocultas de 5 y 4 neuronas
+3 neuronas de salida
+*/
+
+val layers = Array[Int](4, 5, 4, 3)
+
+//7. Construya el modelo de clasificación y explique su arquitectura.
+
+/* Creamos el entrenador estableciendo los parametros correspondientes:
+SetLayers para la estructura de las capas construidas anteriormente
+Setblocksize para el tamano del bloque para los datos de entrada
+El seed o la semiña con su respectivo peso
+Y el maxiter como numero de iteraciones maximas que se realizaran
+*/
+
+val trainer = new MultilayerPerceptronClassifier().setLayers(layers).setBlockSize(128).setSeed(1234L).setMaxIter(100)
+//Inicio del entrenamiento
+val model = trainer.fit(train) 
+val result = model.transform(test) 
+
+// MulticlassClassificationEvaluator recibe dos valores de entrada: prediction y label y como salida muestra la precision del test
+val predictionAndLabels = result.select("prediction", "label")
+val evaluator = new MulticlassClassificationEvaluator().setMetricName("accuracy")
+
+//8. Imprima los resultados del modelo
+
+println(s"\n\nTest set accuracy = ${evaluator.evaluate(predictionAndLabels)}")
